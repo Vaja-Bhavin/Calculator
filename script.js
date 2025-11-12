5ff9792b546842b7bf5aaeeab1bd9552fe88c08f
Vaja-Bhavin <vajabhavin96@gmail.com>
2025-11-12 16:25:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let query = ""
 let ans;
 let complate = false
-let keyValue = ["1","2","3","4","5","6","7","8","9","0"]
-let operator = ["/","*","+","-"]
+let keyValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+let operators = ["/", "*", "+", "-"]
 let lastOp = false;
-document.addEventListener("DOMContentLoaded",()=>{
-    let btns = document.getElementById("buttons")
-    btns.className = btns.className + " animation"
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        let calc = document.getElementById("calc")
+        calc.style.filter = "blur(0px)"
+        setTimeout(() => {
+            calc.style.width = "325px"
+        }, 500);
+    }, 1)
 })
 
 const add = async (value) => {
@@ -16,19 +21,23 @@ const add = async (value) => {
         ans.innerText = ""
         complate = false
     }
-    query = query+value
+    query = query + value
     ans.innerText = ans.innerText + value
     console.log(query)
 }
 
 const addOp = (value) => {
-    query = query+value
-    ans.innerText = ans.innerText + value
-    lastOp = value
-    complate=false
+    try {
+        query = query + value
+        ans.innerText = ans.innerText + value
+        lastOp = value
+        complate = false
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-const equal =  () => {
+const equal = () => {
     let temp;
     try {
         temp = eval(query)
@@ -43,7 +52,7 @@ const equal =  () => {
 
 const backspace = () => {
     query = ans.innerText
-    query = query.substring(0,query.length-1)
+    query = query.substring(0, query.length - 1)
     ans.innerText = query
 }
 
@@ -54,28 +63,40 @@ const clear = () => {
 }
 
 const invert = () => {
-    // if (lastOp) {
-    //   let temp = query
-    //   temp = "("+temp+")" 
-    //   query = temp
-    //   ans.innerText = query
-    // }
-    let temp = query
-    temp = temp.split(lastOp)
-    let temp1 = temp[0] + lastOp+ "(-" + temp[1]+")"
-    query =  temp1
-    ans.innerText = query
+    let temp0 = ans.innerText
+    if (!/[+\-*/]/.test(temp0)) {
+        let temp = query
+        temp = "(" + temp + ")"
+        query = temp
+        ans.innerText = query
+    } else {
+        let temp = query
+        temp = temp.split(lastOp)
+        let temp1 = temp[0] + lastOp + "(-" + temp[1] + ")"
+        query = temp1
+        ans.innerText = query
+    }
 }
 
-document.addEventListener("keypress",(key)=>{
-    console.log(key.key)
-    if (key.key in keyValue) {
-        add(key.key)
-    } else if (key.key in operator) {
-        addOp(key.key)
-    } else if(key.key === "Enter") {
+document.addEventListener("keypress", (key) => {
+    // console.log(key)
+    let k = key.key
+    if (k in keyValue) {
+        add(k)
+    } else if ((/[+\-*/]/.test(k))) {
+        console.log("Hi")
+        addOp(k)
+    } else if (k === "Enter") {
         equal()
-    } else if(key.key === "c") {
+    } else if (k === "c") {
         clear()
-    }
+    } /*else if (k == "+") {
+        addOp("+")
+    } else if (k == "-") {
+        addOp("-")
+    } else if (k == "*") {
+        addOp("*")
+    } else if (k == "/") {
+        addOp("/")
+    }*/
 })
